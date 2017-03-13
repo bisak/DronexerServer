adf10979c8d6225edf21544241bef1cc822a2418
Biser Atanasov <biser.p.atanasov@gmail.com>
2017-03-13 21:22:33 +0200

    Fixed some really stupid promise-related mistakes

--- data/user.data.js
+++ data/user.data.js
@@ -5,27 +5,21 @@ module.exports = (models) => {
 	const User = models.User
 	return {
 		registerUser(newUser){
-			return new Promise((resolve, reject) => {
-				encryption.generateHash(newUser.password)
-					.then((hash) => {
-						newUser.password = hash
-						return User.create(newUser).then(resolve).catch(reject)
-					})
-					.catch((error) => {
-						console.log(error)
-					})
-			})
+			return encryption.generateHash(newUser.password)
+				.then((hash) => {
+					newUser.password = hash
+					return User.create(newUser)
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 		},
 		getUserById(id){
-			return new Promise((resolve, reject) => {
-				User.findById(id).then(resolve).catch(reject)
-			})
+			return User.findById(id)
 		},
 		getUserByUsername(username){
-			return new Promise((resolve, reject) => {
-				const query = { username: username }
-				User.findOne(query).then(resolve).catch(reject)
-			})
+			const query = { username: username }
+			return User.findOne(query)
 		}
 	}
 }
--- util/encryption.util.js
+++ util/encryption.util.js
@@ -2,18 +2,9 @@ const bcrypt = require('bcryptjs')
 
 module.exports = {
 	generateHash(password){
-		return new Promise((resolve, reject) => {
-			bcrypt.genSalt(10, function (err, salt) {
-				bcrypt.hash(password, salt, function (err, hash) {
-					if (err) return reject(err)
-					resolve(hash)
-				});
-			});
-		})
+		return bcrypt.genSalt(10).then(salt => bcrypt.hash(password, salt));
 	},
 	comparePassword(candidatePassword, hash){
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(candidatePassword, hash).then(resolve).catch(reject)
-		})
+		return bcrypt.compare(candidatePassword, hash)
 	}
 }
